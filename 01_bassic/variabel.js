e06a9cadcac8178c9aca15a88c67196fc6affbd8
meetu2053 <121785069+meetu2053@users.noreply.github.com>
2023-01-02 08:41:45 +0000

    variable

---
+++ 01_bassic/variabel.js
@@ -0,0 +1,17 @@
+const accountId=12345
+let accountPassword="123"
+var accountEmail="meet@gmil.com"
+accountCity="kadi"
+let accountState
+/* 
+    not assigne a value of a variable ja declare undifine value ..
+*/
+//accountId=2
+/*
+    do not use var for declaring a variable name use let keyword ....
+*/
+accountEmail="m@gmail.com"
+accountPassword="0"
+accountCity="nagarasan"
+//console.log(accountId);
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
