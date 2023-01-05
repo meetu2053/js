d5ba00c75de819c91d4f81d6f03407f86f400303
meetu2053 <121785069+meetu2053@users.noreply.github.com>
2023-01-05 07:14:42 +0000

    typeconversion

---
+++ 01_bassic/typeconversion.js
@@ -0,0 +1,86 @@
+// number in this case
+let number=33
+console.log(number);
+console.log(typeof number);
+
+
+// string in this case
+let score="33abc"
+console.log(score);
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// NaN means not a number in this case
+
+
+// let try a boolean type in this typeconversion
+
+let a = true
+console.log(typeof a);
+console.log(a);
+let b = Number(a)
+console.log(typeof b);
+console.log(b);
+
+
+
+
+
+
+// let try to pass null value and cheack the output
+
+
+let name = null
+console.log(typeof name);
+console.log(name);
+let lastName = Number(name)
+console.log(typeof lastName); 
+console.log(lastName);
+
+
+// try in undifine datatype
+
+
+let state;
+console.log(typeof state);
+console.log(state);
+let s= Number(state)
+console.log(state);
+console.log(s);
+
+
+console.log("meet");
+// let try to convert number,string to boolean 
+
+    let lodded = 1
+    console.log(typeof lodded);
+    let booleanLoged = Boolean(lodded)
+    console.log(typeof booleanLoged);
+    console.log(booleanLoged);
+
+
+    let n = "meet"
+    console.log(typeof n);
+    let m = Boolean(n)
+    console.log(typeof m);
+    console.log(m);
+    
+
+    
+    let x = ""
+    console.log(typeof x);
+    let y = Boolean(x)
+    console.log(typeof y);
+    console.log(y);
+    
+
+
+    // output
+    // number
+    // 1 >= true; 0>= false;
+    // string
+    // "meet" >= true;
+    // "">= false;
+    
