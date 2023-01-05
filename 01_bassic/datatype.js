2475366b2b41b7265f5733c2e5975932f1bae63a
meetu2053 <121785069+meetu2053@users.noreply.github.com>
2023-01-05 06:46:15 +0000

    datatype

---
+++ 01_bassic/datatype.js
@@ -0,0 +1,25 @@
+let score = 33
+let name = "meet"
+console.log(typeof score)
+console.log(score);
+console.log(typeof name);
+console.log(name);
+
+BigInt=5252525252525252525252525252525252
+console.log(BigInt);
+/*
+    number >=2 poewr of 53
+    BigInt
+    string
+    boolean >= true or fluse
+    
+
+*/
+
+let age=null
+console.log(typeof age);
+// null >special type null datatype ni type object hoy che 
+
+let state;
+console.log(state);
+// undifine > it undifine 
